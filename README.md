1f70bbd9a7614c79da92a945ff80edc3811b4880
Michael Lawlor <mike@auka.ca>
2020-01-27 18:50:00 +0700

    First working version of logger, version 0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # capacitor-native-log
+
+### Notes:
+
+1. Make sure your XCode scheme does not have the `OS_ACTIVITY_MODE` environment variable set to `disable` (Product > Scheme > Edit Scheme > Arguments > Environment Variables)
+
+2. In Console.app, make sure that the info and debug messages are visible: Action > Include Info Messages / Include Debug Messages
+
+3. If you're still not seeing the messages, try ensuring they're enabled for your app:
+```
+sudo log config --subsystem your.bundle-id --mode level:debug
+```
+
+4. If you want to persist the messages instead of them simply remaining in memory:
+```
+sudo log config --subsystem your.bundle-id --mode level:debug,persist:debug
+```
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -2,28 +2,17 @@ import Foundation
 import os.log
 import Capacitor
 
-extension OSLog {
-//    private static var subsystem = Bundle.main.bundleIdentifier!
-    private static var subsystem = "app.play-time"
-    private static var category = "CAPNativeLog"
-}
-
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitor.ionicframework.com/docs/plugins/ios
- */
 @objc(CAPNativeLog)
 public class CAPNativeLog: CAPPlugin {
+    static var logTarget = OSLog(subsystem: Bundle.main.bundleIdentifier!, category: "CAPNativeLog")
+    
     @objc func log(_ call: CAPPluginCall) {
-        NSLog("CAPNativeLog NSLog")
-        os_log("CAPNativeLog os_log", log: OSLog.default, type: OSLogType.error)
         let message = call.getString("message") ?? ""
         let level = call.getString("level") ?? "debug"
         writeToLog(level: level, message: message)
     }
     
     internal func writeToLog(level: String, message: String) {
-        NSLog("CAPNativeLog writeToLog [\(level)] - \(message)")
         let type: OSLogType
         switch level.lowercased() {
             case "debug":
@@ -37,6 +26,6 @@ public class CAPNativeLog: CAPPlugin {
             default:
                 type = OSLogType.default
         }
-        os_log("%{public}@", log: OSLog.default, type: type, message)
+        os_log("%{public}@", log: CAPNativeLog.logTarget, type: type, message)
     }
 }
